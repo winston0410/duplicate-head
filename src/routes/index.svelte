dd17cc77fef14311354b95377303e191b8faee9e
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-04-11 09:04:31 +0100

    Reproduce

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+    import { mkSchema } from '$lib/jsonld'
+</script>
+
+<script lang="ts">
+    let orgSchema = {
+        "@context": "https://schema.org/",
+		"@type": "Organization",
+    }
+</script>
+
+<svelte:head>
+    {@html mkSchema(orgSchema)}
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
